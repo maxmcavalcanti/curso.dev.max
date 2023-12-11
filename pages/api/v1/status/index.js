699701e87cd56a9217256b4d1d687c731f63ba83
Maximiliano Muñoz Cavalcanti <95191637+maxmcavalcanti@users.noreply.github.com>
2023-12-11 14:02:22 +0000

    add `jsconfig.json`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,7 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
+  const result = await database.query("SELECT 1+2 as sum;");
   console.log(result.rows);
   response.status(200).json({ chave: "valor" });
 }
