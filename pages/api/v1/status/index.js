e3b19032d8d9a54e9e3f681bea38a91e2bb083f0
Maximiliano Muñoz Cavalcanti <95191637+maxmcavalcanti@users.noreply.github.com>
2024-01-03 12:54:25 +0000

    finish endpoit `status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -4,9 +4,11 @@ async function status(request, response) {
   const updatedAt = new Date().toISOString();
   const postgresVersion = await database.query("SHOW server_version;");
   const maxConnections = await database.query("SHOW max_connections;");
-  const currentConnections = await database.query(
-    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';",
-  );
+  const databaseName = process.env.POSTGRES_DB;
+  const currentConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
   const result = {
     updated_at: updatedAt,
     dependencies: {
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -21,3 +21,9 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(responseBody.dependencies.database.max_connections).toBe(100);
   expect(responseBody.dependencies.database.opened_connections).toBe(1);
 });
+
+// test.only("Teste de SQL Injections", async () => {
+//   await fetch(
+//     "http://localhost:3000/api/v1/status?databaseName='; SELECT pg_sleep(4) --",
+//   );
+// });
